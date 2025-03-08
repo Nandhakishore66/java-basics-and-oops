252fdf9ac18408f4b878ccaac8b2db55e461edd0
Nandhakishore66 <nandhakishore.ravi66@gmail.com>
2025-03-08 14:47:48 +0530

    Add files via upload

--- src/com/naveen/StringUserInput.java
+++ src/com/naveen/StringUserInput.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class StringUserInput {
     public static void main(String[] args) {
-       String multipleWord;
-      String singleWord;
-//
-//
-//        //Getting Input from user
+        String multipleWord;
+        String singleWord;
+        char val;
+        
+       //Getting Input from user
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a multiple word");
         //If i want to enter a sentence or double name use nextLine
@@ -18,8 +18,13 @@ public class StringUserInput {
         singleWord = input.next();
 
         System.out.println("Entered name" + multipleWord);
-        System.out.println("Entered name" + singleWord);
+        System.out.println("Entered" +
+                " name" + singleWord);
 
+        //char input example
+        val = input.next().charAt(2);
+        System.out.println("Enter a char");
+        System.out.println("Entered char" + val);
 
     }
 }
